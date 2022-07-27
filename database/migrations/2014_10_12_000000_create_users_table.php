56fc220ed804b7fb4f9b37596c6f03dd8c240b57
abbnggaa <abbnggaa@gmail.com>
2022-07-27 23:37:17 +0700

    nambahin column buat nik

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->bigInteger('nik')->nullable();
             $table->string('name');
             $table->string('email')->unique();
             $table->boolean('role')->nullable();
