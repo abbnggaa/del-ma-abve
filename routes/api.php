726531c0198c5b3e4bdae66d1f3416de53a52876
abbnggaa <abbnggaa@gmail.com>
2022-08-19 11:20:47 +0700

    login

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\CatatanPerjalananController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,5 +17,3 @@ use App\Http\Controllers\Api\CatatanPerjalananController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::apiResource('/catatanperjalanan', CatatanPerjalananController::class);
